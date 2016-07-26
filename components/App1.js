648b61b9054f9e74f54d6346d451df9e67dfec08
Micky Shaked <micky.shaked@gmail.com>
2016-07-25 21:11:06 -0700

    trying again with webpack

---
+++ components/App1.js
@@ -0,0 +1,18 @@
+import {  } from 'react-bootstrap';
+import React, {Component} from 'react';
+import {render} from 'react-dom';
+import LoginBox from './LoginBox';
+import Data from './Data';
+
+
+export default class App extends Component {
+	render() {
+
+		return (
+			<div id='App'>
+				<LoginBox />
+				<Data />
+			</div>
+		)
+	}
+}
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = {
+  entry: ["babel-polyfill", "./components/app.js"],
+  output: {
+      path: __dirname + '/Build',
+      // publicPath: '/assets/',
+      filename: "bundle.js"
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  module: {
+      loaders: [
+          {
+            test: /\.css$/,
+            loaders: ['style', 'css']
+          },
+          {
+            test: /\.scss$/,
+            loaders: ['style', 'css', 'sass']
+          },
+          {
+            test: /\.jsx?$/,
+            loader: 'babel-loader',
+            exclude: /(node_modules)/,
+            query: {
+              plugins: ['transform-runtime'],
+              presets: ['es2015', 'react']
+            }
+          }
+      ]
+  }
+};
