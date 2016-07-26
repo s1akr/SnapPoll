18c5f4b5d910dc81a5584affecfac17f9fc36bc4
Mike Puangmalai <mike@nongaap.com>
2016-07-26 11:16:00 -0700

    Prelim routes added to server.js file. Database schema added to PollModel.js. Route handling and data saving/sending middleware built. Added vote.html and results.html to folder.

--- database/Models/PollModel.js
+++ database/Models/PollModel.js
@@ -1,25 +1,41 @@
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
+// var mongoose = require('mongoose');
+// var Schema = mongoose.Schema;
 
 
-var pollSchema = new Schema({
-  hostOwner: String,
-  Title: String,
-  accessCode: String,
-  pollQuestionMeta: {
-    title: String,
-    questions: [String],
-    results: [Number]
-  },
-  isActive: Boolean,
-  created_at: Date,
-  updated_at: Date
+// var pollSchema = new Schema({
+//   hostOwner: String,
+//   Title: String,
+//   accessCode: String,
+//   pollQuestionMeta: {
+//     title: String,
+//     questions: [String],
+//     results: [Number]
+//   },
+//   isActive: Boolean,
+//   created_at: Date,
+//   updated_at: Date
+// });
+
+
+// // the schema is useless so far
+// // we need to create a model using it
+// var Poll = mongoose.model('Poll', pollSchema);
+
+// // make this available to our users in our Node applications
+
+
+//MP - DB schema code for iteration project
+'use strict';
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const pollSchema = new Schema({
+  question: String,
+  choices: [String],
+  answers: [String],
+  counter: [Number]
 });
 
+const Poll = mongoose.model('Poll', pollSchema);
 
-// the schema is useless so far
-// we need to create a model using it
-var Poll = mongoose.model('Poll', pollSchema);
-
-// make this available to our users in our Node applications
 module.exports = Poll;
