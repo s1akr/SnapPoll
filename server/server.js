adc589e0957a9b64b0feec9bb11d822ca325e955
Chandra Lindy <chandra.lindy@gmail.com>
2016-07-26 16:55:10 -0700

    fix route and test

--- server/server.js
+++ server/server.js
@@ -23,18 +23,18 @@ app.listen(8080, function() {
 //MP - routes code for iteration project
 // Confirm file structure and file path with team. Currently commented out.
 
-app.get('/', function(req,res) {
+app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname,'../','vote.html'));
 });
 
-app.get('/results', function(req,res) {
+app.get('/results', function(req, res) {
   res.sendFile(path.join(__dirname,'../','results.html'));
 });
 
 app.get('/data', dbMethods.getPoll);
 
-app.post('/data', dbMethods.countAnswer, function(req,res) {
-  res.redirect('/results.html');
+app.post('/data', dbMethods.countAnswer, function(req, res) {
+  res.redirect('/results');
 });
 
 module.exports = app;
